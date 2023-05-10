08f2028e3cc0d4995ae1021e9a72523af68b6b96
amirhasrati <hasratiamirhossein@gmail.com>
2023-05-10 18:15:34 -0400

    Javascript Review

---
+++ section-16/Conditionals/script.js
@@ -0,0 +1,45 @@
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!");
+// } else {
+//     console.log("YOUR NUMBER IS GREATER (OR EQUAL) THAN 0.5!!!");
+// }
+// console.log(random);
+
+// const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+
+// if (dayOfWeek === 'monday') {
+//     console.log("UGHHHH I HATE MONDAYS!");
+// } else if (dayOfWeek === 'saturday') {
+//     console.log("YAY I LOVE SATURDAYS!");
+// } else if (dayOfWeek === 'friday') {
+//     console.log("FRIDAYS ARE DECENT, ESPECIALLY AFTER WORK!");
+// } else {
+//     console.log("MEH");
+// }
+
+// const age = 89;
+
+// if (age < 5) {
+//     console.log("You're a baby. You get in for free!");
+// } else if (age < 10) {
+//     console.log("You are a child. You pay $10");
+// } else if (age < 65) {
+//     console.log("You are an adult. You pay $20");
+// } else {
+//     console.log("You are a senior. You pay $10");
+// }
+
+const password = prompt("Please enter a new password");
+
+// Password must be 6+ characters
+if (password.length >= 6) {
+    // Password cannot include space
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid Password!");
+    } else {
+        console.log("Password cannot contain spaces!");
+    }
+} else {
+    console.log('PASSWORD TOO SHORT! Must be 6+ characters');
+}
