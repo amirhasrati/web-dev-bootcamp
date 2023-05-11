f09ee02dae55b186f9abd253f54ad108d9667997
amirhasrati <hasratiamirhossein@gmail.com>
2023-05-11 15:05:03 -0400

    Update

---
+++ section-21/FunctionExpressions/script.js
@@ -0,0 +1,8 @@
+// function add(x, y) {
+//     return x + y;
+// }
+
+// storing a function with NO name inside of a variable
+const add = function (x, y) {
+    return x + y;
+}
---
+++ section-21/Scope/script.js
@@ -0,0 +1,49 @@
+// let totalEggs = 0;
+
+// function collectEggs() {
+//     totalEggs = 6;
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     // let bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
+
+// birdWatch();
+// console.log(bird);
+
+// let radius = 8;
+// if (radius > 0) {
+//     const PI = 3.14159;
+//     let msg = 'HIII!';
+// }
+
+// console.log(radius);
+// console.log(PI);
+
+// for (var i = 0; i < 5; i++) {
+//     var msg = "AGAJDGKLJAD";
+//     console.log(msg);
+// }
+// console.log(msg);
+// console.log(i);
+
+function bankRobbery() {
+    const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Bat Woman'];
+    function cryForHelp() {
+
+        function inner() {
+            for (let hero of heroes) {
+                console.log(`PLEASE HELP US, ${hero.toUpperCase()}`);
+            }
+        }
+
+        inner()
+    }
+
+    cryForHelp();
+}
