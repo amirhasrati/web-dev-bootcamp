3a18c89da146dea11b3089ceddec8829d2343bdb
amirhasrati <hasratiamirhossein@gmail.com>
2023-05-20 02:09:43 -0400

    Section 25 Completed

--- section-24/Pokemon/app.js
+++ section-24/Pokemon/app.js
@@ -3,7 +3,6 @@
 const container = document.querySelector('#container');
 const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
-
 for (let i = 1; i < 152; i++) {
     const pokemon = document.createElement('div');
     pokemon.classList.add('pokemon');
