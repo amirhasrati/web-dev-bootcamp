f57325fda54869625a38175cab895faa22498e44
Amir Hasrati <hasratiamirhossein@gmail.com>
2023-07-21 09:41:05 -0400

    Update

---
+++ section-23/DefaultParams/app.js
@@ -0,0 +1,7 @@
+function rollDie(numSides = 6){
+    return Math.floor(Math.random() * numSides) + 1;
+}
+
+function greet(person, msg = "Hey there", punc = "!") {
+    console.log(`${msg}, ${person}${punc}`);
+}
---
+++ section-23/Spread/app.js
@@ -0,0 +1,6 @@
+const nums = [13, 4, 5, 21,3,3,8,9,10,92,4];
+
+const max = Math.max(...nums);
+
+console.log(nums);
+console.log(...nums);
