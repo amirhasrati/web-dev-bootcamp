665714d9a5647458d22b69c7eeab8691b61d3494
Amir Hasrati <ahasrati@uoguelph.ca>
2024-02-02 00:23:50 -0500

    U in CRUD

--- section-38/Mongoose_Express/index.js
+++ section-38/Mongoose_Express/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-
+const methodOverride = require("method-override");
 const Product = require("./models/product");
 
 // Mongoose Connection
@@ -16,10 +16,11 @@ mongoose
         console.log("MONGO: Oh No! Error!");
     });
 
-// Views and view engine
+// Middleware
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // Routes
 app.get("/products", async (req, res) => {
@@ -41,6 +42,21 @@ app.get("/products/:id", async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+        runValidators: true,
+        new: true,
+    });
+    res.redirect(`/products/${product._id}`);
 });
 
 // Port
